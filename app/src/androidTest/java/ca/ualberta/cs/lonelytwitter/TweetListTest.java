e1e32991b0ff61f24dc60f2f30ae2926d677d5cf
Kirsten Kwong <kskwong@ualberta.ca>
2017-02-16 17:27:43 -0700

    lab6

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -1,8 +1,31 @@
 package ca.ualberta.cs.lonelytwitter;
 
+import android.test.ActivityInstrumentationTestCase;
+import android.test.ActivityInstrumentationTestCase2;
+
 /**
  * Created by kirsten on 2/16/17.
  */
 
-public class TweetListTest {
+public class TweetListTest extends ActivityInstrumentationTestCase2 {
+
+    public TweetListTest(){
+        super(LonelyTwitterActivity.class);
+
+    }
+
+    public void testAddTweet(){
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("some tweet");
+        tweets.add(tweet);
+        assertTrue(tweets.hasTweet(tweet));
+    }
+
+    public void testHasTweets(){
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("some tweet");
+        assertFalse(tweets.hasTweet(tweet));
+        tweets.add(tweet);
+        assertTrue(tweets.hasTweet(tweet));
+    }
 }
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -1,8 +1,20 @@
 package ca.ualberta.cs.lonelytwitter;
 
+import java.util.ArrayList;
+
 /**
  * Created by kirsten on 2/16/17.
  */
 
 public class TweetList {
+    private ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+
+    public void add(Tweet tweet){
+        tweets.add(tweet);
+    }
+
+    public boolean hasTweet(Tweet tweet){
+        return tweets.contains(tweet);
+
+    }
 }
